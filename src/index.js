0fa52ecfaeca74280e9bacb6f06b670bdb6f54dd
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-12-13 15:24:16 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -36,10 +36,12 @@ function onSearchSubmit(e) {
   fetchContent(inputSearch, page, amountContent)
     .then(showGallery)
     .catch(onError);
+  onPageSelect(amountContent);
 }
 
 // function show img content
 function showGallery(dataOwner) {
+  onPageSelect(amountContent);
   const markup = galleryRender(dataOwner);
   refs.gallery.insertAdjacentHTML('beforeend', markup);
   scrollSmooth();
@@ -61,7 +63,6 @@ function showGallery(dataOwner) {
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
-  onPageSelect(amountContent);
 }
 
 // function error
